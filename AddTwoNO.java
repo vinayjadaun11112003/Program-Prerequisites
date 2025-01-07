f2507118a011e433e0eb7b0205b3a29d8e5e0f40
vinay_jadaun <vinay11112003@gmail.com>
2025-01-07 23:05:36 +0530

    Assited Problem 02 Add Two Numbers

---
+++ AddTwoNO.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class AddTwoNO {
+    public static void main(String[] args){
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please Enter first Number:- ");
+        int a=s.nextInt();
+        System.out.println("Please Enter second no: ");
+        int b=s.nextInt();
+        System.out.println("The sum of two number are: ");
+        System.out.print(a+b);
+    }
+}
